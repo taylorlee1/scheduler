5bcf06da66da49535e6df1bfd39e352561a929d8
Taylor Lee <taylorleebiz@gmail.com>
2017-09-12 15:44:13 -0700

    implemented checkboxes

--- html/js/scheduler.js
+++ html/js/scheduler.js
@@ -1,4 +1,196 @@
 
+Vue.component('sched-trh', {
+    template: '\
+        <tr>\
+            <th></th>\
+            <sched-th\
+                v-for="day in days"\
+                v-bind:key="day.name"\
+                v-bind:dayname="day.name"\
+            ></sched-th>\
+        </tr>\
+        ',
+    props: [ 'days'],
+    computed: {
+    },
+    methods: {
+    },
+    });
+
+Vue.component('sched-trd', {
+    template: '\
+        <tr>\
+            <td class="small-font">{{ humanTime }}</td>\
+            <sched-td\
+                v-for="day in days"\
+                v-bind:key="day.name"\
+                v-bind:dayname="day.name"\
+                v-bind:time="time"\
+                v-bind:tableid="tableid"\
+            ></sched-td>\
+        </tr>\
+        ',
+    props: ['time', 'days', 'tableid'],
+    computed: {
+        humanTime: function() {
+            var timeStr = (this.time >= 1300) ? 
+                (this.time - 1200).toString() + 'pm' : this.time.toString() + 'am';
+            
+            return timeStr.toString().slice(0,-4) + 
+                    ':' + timeStr.toString().slice(-4);
+        },
+    },
+    methods: {
+    },
+    });
+
+
+Vue.component('sched-td', {
+    template: '\
+        <td><input-check\
+                v-bind:inputid="inputid"\
+                ></input-check></td>\
+        ',
+    props: ['dayname', 'time', 'tableid'],
+    computed: {
+        inputid: function() {
+            return { tableid: this.tableid, dayname: this.dayname, time: this.time }
+        },
+    },
+    methods: {
+    },
+    });
+
+Vue.component('input-check', {
+    template: '\
+        <input type="checkbox" v-model="boolState" v-on:change="emit">\
+        ',
+    props: ['daytime', 'inputid'],
+    data: function() {
+        return { boolState: false }
+    },
+    methods: {
+        emit: function() {
+            console.log("checked " + JSON.stringify(this.inputid) + this.boolState);
+            this.$emit('checked', JSON.stringify(this.inputid) + this.boolState);
+        }
+    },
+
+    });
+Vue.component('sched-th', {
+    template: '\
+        <th>{{ dayname }}</th>\
+        ',
+    props: ['dayname'],
+    computed: {
+    },
+    methods: {
+    },
+    });
+
+
+/*
+Vue.component('timeslot', {
+    template: '\
+        <div class="time-slot">\
+            {{ humanTime }}\
+        </div>\
+        ',
+    props: ['time'],
+    computed: {
+        humanTime: function() {
+            var timeStr = (this.time >= 1300) ? 
+                (this.time - 1200).toString() + 'pm' : this.time.toString() + 'am';
+            
+            return timeStr.toString().slice(0,-4) + 
+                    ':' + timeStr.toString().slice(-4);
+        },
+    },
+    methods: {
+    },
+    });
+*/
+/*
+Vue.component('day-column', {
+    template: '\
+        <div class="flex-item">\
+            <span> {{ dayname }} </span>\
+            <timeslot\
+                v-if="showtimes"\
+                v-for="t in Times"\
+                v-bind:key="t.time"\
+                v-bind:time="t.time"\
+            ></timeslot>\
+            <div class="talldiv">\
+                <textarea class="filltextbox"\
+                    v-if="!showtimes"\
+                    v-model="textBoxComment"\
+                    v-on:keyup.enter="setTextBoxComment"\
+                    placeholder="add comment (optional)"\
+                \></textarea>\
+            </div>\
+        </div>\
+        ',
+    data: function() {
+        T = []
+        for (i=700; i< 2000; i=i+100) {
+            T.push({ time: i });
+            T.push({ time: i+30 });
+        }
+        return { Times : T, textBoxComment : '' };
+
+    },
+    props: ['dayname','showtimes'],
+    methods: {
+        setTextBoxComment: function() {
+            console.log("component.day-column() start");
+            this.$emit('enter', this.textBoxComment);
+            console.log("component.day-column() " + this.textBoxComment);
+        },
+    },
+    });
+*/
+
+Vue.component('schedule-listing', {
+    template: '\
+        <div>\
+            <span class="schedule" v-on:click="goto_"> {{ title }} </span>\
+            <button v-on:click="edit_">edit</button>\
+            <button v-on:click="remove_">remove</button>\
+        </div>\
+        ',
+    props: ['title'],
+    methods: {
+        goto_ : function() {
+            this.$emit('goto');
+        },
+        edit_ : function() {
+            this.$emit('edit');
+        },
+        remove_ : function() {
+            this.$emit('remove');
+        }
+    },
+    });
+
+function showArray(arr) {
+    console.log("showArray()");
+    for (var i = 0; i < arr.length; ++i) {
+        console.log(i + '=' + JSON.stringify(arr[i]));
+    }
+};
+
+function httpGet(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open( "GET", theUrl, true);
+    xmlHttp.send( null );
+};
+
 var app = new Vue({
     el: '#app',
     data: {
@@ -6,30 +198,250 @@ var app = new Vue({
         homeVisible: false,
         scheduleVisible: false,
         userId: '',
+        userValid: false,
+        realName: 'DEFAULT_USER_NAME',
+        newScheduleName: '',
+        displayScheduleName: {
+            id: false,
+            name: false,
+        },
+        editRealName: false,
+        addNewScheduleDialog: 'add a new schedule',
+        textBoxComment: '',
+        Schedules: [],
+        Availabilities: [],
+        CheckedBoxes: [],
+        //Days: ['sun','mon','tue','wed','thu','fri','sat'],
+
+        
+        Days: [
+                {   name: 'sun'.toUpperCase(),
+                    color: '#7FFF00',
+                    showtimes: 1,
+                },
+                {
+                    name: 'mon'.toUpperCase(),
+                    color: '#DCDCDC',
+                    showtimes: 1,
+                },
+                {
+                    name: 'tue'.toUpperCase(),
+                    color: '#D2691E',
+                    showtimes: 1,
+                },
+                {
+                    name: 'wed'.toUpperCase(),
+                    color: '#FFF8DC',
+                    showtimes: 1,
+                },
+                {
+                    name: 'thu'.toUpperCase(),
+                    color: '#00FFFF',
+                    showtimes: 1,
+                },
+                {
+                    name: 'fri'.toUpperCase(),
+                    color: '#008B8B',
+                    showtimes: 1,
+                },
+                {
+                    name: 'sat'.toUpperCase(),
+                    color: '#8FBC8F',
+                    showtimes: 1,
+                },
+                ],
+
+
+        Times: [],
+
     },
+    computed: {
+        DaysIndented: function() {
+            return [{ name: this.realName, color: '#FFFFFF', showtimes: 0}].concat(this.Days);
+        },
+    },
+
     methods: {
+        //
+        // methods LOGIN start
+        //
         isUserIdValid: function() {
             console.log("isUserIdValid() start");
-            return false;
+            httpGet('http://127.0.0.1:5000/validateUser?id=' + this.userId,
+                this.setUserValid
+                );
+        },
+    
+        setUserValid: function(data) { //valid, realName) {
+            console.log("setUserValid() " + data);
+            data = JSON.parse(data);
+            console.log("setUserValid() " + data);
+            var valid = data.valid;
+            var realName = data.realName;
+            this.userValid = (valid === true);
+            if (this.userValid) {
+                localStorage.setItem('userId', this.userId);
+                this.realName = realName;
+                localStorage.setItem('realName', this.realName);
+            } else {
+                localStorage.setItem('userId', '');
+                this.userId = '';
+            }
+            console.log("setUserValid() userId: " + 
+                localStorage.getItem('userId'));
+            this.checkUserStatus();
+        },
+
+        setUserId: function(id) {
+            this.userId = id;
         },
 
         loginUser: function() {
-            if (this.isUserIdValid()) {
-                localStorage.setItem('userId', this.userId);
-                console.log("loginUser() userId: " + 
-                    localStorage.getItem('userId'));
-            } else {
-                console.log("loginUser() userId " + " not valid");
-            }
+            this.isUserIdValid();
         },
 
         getNewId: function() {
+            this.userId = 'generating...';
+            httpGet('http://127.0.0.1:5000/generateNewUser', this.setUserId);
         },
+
+        checkUserStatus: function() {
+            console.log("checkUserStatus() user valid = " + this.userValid);
+            if (this.userValid) {
+                this.updateSchedules();
+                this.loginVisible = false;
+                this.homeVisible = true;
+            } else {
+                this.loginVisible = true;
+                this.homeVisible = false;    
+            }
+        },
+
+        //
+        // methods LOGIN end
+        //
+
+        //
+        // methods HOMEPAGE start
+        //
+        removeSchedule: function(sched) {
+            console.log("removeSchedule() " + sched.schedid);
+            httpGet('http://127.0.0.1:5000/rmSchedule?schedid=' + 
+                sched.schedid + '&id=' + this.userId, 
+                this.updateSchedules);
+
+        },
+
+        editSchedule: function(sched) {
+            console.log("editSchedule() " + sched.schedid);
+        },
+
+        gotoSchedule: function(sched) {
+            console.log("gotoSchedule() " + sched);
+            this.displaySchedule = sched;
+            this.loginVisible = false;
+            this.homeVisible = false;
+            this.scheduleVisible = true;
+        },
+
+        emptySchedule: function() {
+            console.log("emptySchedule()");
+            this.displayScheduleName = false;
+            this.loginVisible = false;
+            this.homeVisible = true;
+            this.scheduleVisible = false;
+        },
+
+        addNewSchedule: function() {
+            console.log("addNewSchedule() " + this.newScheduleName);
+            httpGet('http://127.0.0.1:5000/addSchedule?name=' + 
+                this.newScheduleName + '&id=' + this.userId, 
+                this.updateSchedules);
+            document.activeElement.blur();
+            this.newScheduleName = '';
+        },
+
+        updateSchedules: function() {
+            console.log("updateSchedules()");
+            httpGet('http://127.0.0.1:5000/getSchedules?id=' + this.userId,
+                this.setSchedules);
+        },
+        
+        setSchedules: function(data) {
+            console.log("setSchedules() " + data);
+            data = JSON.parse(data);
+            console.log("setSchedules() " + data);
+            this.Schedules = [];
+            for (var i = 0; i < data.length; ++i) {
+                this.Schedules.push({ 
+                    'name' : data[i].name,
+                    'schedid' : data[i].schedid,
+                });
+            }
+            console.log("setSchedules() Schedules length: " + data.length);
+            console.log("setSchedules() Schedules:");
+            showArray(this.Schedules);
+        }, 
+        
+        setEditRealName: function() {
+            this.editRealName = true;
+        },
+
+        updateRealName: function() {
+            console.log("updateRealName() start");
+            this.editRealName = false; 
+            httpGet('http://127.0.0.1:5000/updateRealName?name=' + 
+                this.realName+ '&id=' + this.userId, this.updateRealNameCallback);
+        },
+        
+        updateRealNameCallback: function(foo) {
+            console.log("updateRealNameCallback " + foo); 
+            this.realName = foo;
+            console.log("updateRealNameCallback done");
+            localStorage.setItem('realName', this.realName);
+        },
+
+        clearLocalStorage: function() {
+            localStorage.clear();
+            location.reload();
+        },
+
+        setTextBoxComment: function(comment) {
+            console.log("setTextBoxComment() " + comment);
+        },
+        tebow: function(comment) {
+            console.log("tebow() " + comment);
+        },
+        //
+        // methods HOMEPAGE end
+        //
+
+        //
+        // methods SCHEDULE start
+        //
+
+        addAvailability: function() {
+            console.log("addAvailability() start");
+            this.Availabilities.push({});
+        }
+        //
+        // methods SCHEDULE end
+        //
     },
+
+    components: {
+        //
+        // components HOMEPAGE start
+        //
+
+    },
+
 
     beforeCreate: function() {
         console.log("beforeCreate() start");
         console.log("beforeCreate() end");
+    
+
     },
 
     created: function() {
@@ -38,12 +450,22 @@ var app = new Vue({
             console.log("created(): no userId stored");
             this.loginVisible = true;
         } else {
-            console.log("created() userId: " + localStorage.getItem('userId'));
-            this.loginVisible = false;
+            this.userId = localStorage.getItem('userId');
+            this.realName = localStorage.getItem('realName');
+            console.log("created(): userId exists: " + this.userId);
+            this.isUserIdValid();
+            //this.loginVisible = false;
+            //this.homeVisible = true;
         }
         console.log("created() done");
         document.getElementById('appWrapper').style.visibility = 'visible';
-        
+
+
+        for (i=700; i< 2000; i=i+100) {
+            this.Times.push({ time: i });
+            this.Times.push({ time: i+30 });
+        }
+
     },
 
 });
--- scheduler.py
+++ scheduler.py
@@ -1,6 +1,198 @@
+import uuid
+import os
+import binascii
+import string
+import pprint
 from flask import Flask
+from flask import request
+from flask_cors import CORS
+import base64
+import pymongo
+import json
+from bson.objectid import ObjectId
+
+import logging
+fmt = '%(asctime)s %(name)s.%(funcName)s +%(lineno)s: ' + \
+    '%(levelname)-8s [%(process)d] %(message)s'
+logging.basicConfig(
+    level=logging.DEBUG,
+    #format=fmt,
+    )
+
 app = Flask(__name__)
+CORS(app)
+
+def randomString():
+    x = binascii.hexlify(os.urandom(16)).decode('utf8')
+    return x
+
+class DB():
+    def __init__(self):
+        self.client = pymongo.MongoClient()
+        self.db = self.client.scheduler
+        self.users = self.db.users
+        self.schedules = self.db.schedules
+        logging.info("DB: db initialized")
+
+    def newUser(self):
+        user = {
+            'userId' : randomString(),
+            'realName' : 'DEFAULT_USER_NAME',
+            'schedules' : [],
+            }
+        userDbId = self.users.insert_one(user).inserted_id
+        newuser = self.users.find_one({'userId' : user['userId']})
+        logging.info("DB: newUser %s" % pprint.pprint(newuser))
+        return newuser['userId']
+
+    def getUser(self, userId):
+        tmp = {'userId' : userId}
+        faf = self.users.find_one(tmp)
+        return faf
+
+    def userExists(self, userId):
+        u = self.getUser(userId)
+        logging.info("DB: userExists() %s" % (u))
+        if u:
+            return { 'valid' : True, 'realName': u['realName'] };
+        else:
+            return { 'valid' : False, 'realName': 'dont_care' };
+  
+    def updateRealName(self, userId, name):
+        user = {
+            'userId' : userId,
+        }
+        result = self.users.update_one(
+            {"userId" : userId},
+            {"$set" : { 'realName' : name } },
+            )
+        logging.info("DB: updateRealName() %s %s" % (
+            result.matched_count,
+            result.modified_count,
+            )) 
+        tmp = {'userId' : userId}
+        faf = self.users.find_one(tmp)
+        logging.info("DB: updateRealName() %s" % (faf))
+        return faf['realName']
+
+    def addSchedule(self, userId, sname):
+        schedule = {
+            'name' : sname,
+            'owner' : userId,
+            'schedid' : randomString(),
+        }
+        scheduleDbId = self.schedules.insert_one(schedule).inserted_id
+        result = self.users.update_one(
+            {"userId" : userId},
+            {"$push" : { 'schedules' : { 'name' : schedule['name'], 'schedid' : schedule['schedid'] } } },
+            )
+        logging.info("DB: addSchedule() %s %s" % (
+            result.matched_count, 
+            result.modified_count,
+            ))
+        
+        return str(scheduleDbId);
+  
+    def rmSchedule(self, userId, schedid):
+        # try to rm assuming you're the owner
+        schedule = {
+            'schedid' : schedid,
+            'owner' : userId,
+        }
+        logging.info("DB: rmSchedule() %s" % (schedule))
+        result0 = self.schedules.delete_one(schedule)
+        logging.info("DB: rmSchedule() %s" % (
+            result0.deleted_count,
+            ))
+        
+        # then remove from your own schedule list
+        result1 = self.users.update_one(
+            {"userId" : userId},
+            {"$pull" : { 'schedules' : { 'schedid' : schedid } } },
+            )
+
+        logging.info("DB: rmSchedule() %s %s" % (
+            result1.matched_count, 
+            result1.modified_count,
+            ))
+        
+        return 'ok'
+#(result0.deleted_count * result1.modified_count)
+
+    def getSchedules(self, userId):
+        result = self.users.find_one({'userId' : userId})
+        logging.info("DB: getSchedules() %s" % (result))
+        return result['schedules']
+
+
+class User():
+    db = DB()
+
+    def __init__(self,userId=None):
+        if userId == None:
+            self.userId = User.db.newUser()
+            self.schedules = []
+        else:
+            self.userId = userId 
+        
+    def isValid(self):
+        return User.db.userExists(self.userId)
+        
+    def addSchedule(self, sname):
+        return User.db.addSchedule(self.userId, sname)
+
+    def rmSchedule(self, schedid):
+        return User.db.rmSchedule(self.userId, schedid)
+
+    def getSchedules(self):
+        return User.db.getSchedules(self.userId)
+
+    def updateRealName(self, name):
+        return User.db.updateRealName(self.userId, name);
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/generateNewUser')
+def newAcct():
+    newUser = User()
+    return newUser.userId
+
+@app.route('/validateUser')
+def validateUser():
+    id_ = request.args.get('id')
+    u = User(id_)
+    resp = u.isValid()
+    logging.info("validateUser() %s" % (resp))
+    return json.dumps(resp)
+
+@app.route('/updateRealName')
+def updateRealName():
+    id_ = request.args.get('id')
+    realName = request.args.get('name')
+    u = User(id_)
+    resp = u.updateRealName(realName)
+    logging.info("updateRealName() %s" % (resp))
+    return resp
+
+
+@app.route('/addSchedule')
+def addSchedule():
+    name = request.args.get('name')
+    id_ = request.args.get('id')
+    u = User(id_)
+    return u.addSchedule(name)
+
+@app.route('/getSchedules')
+def getSchedules():
+    id_ = request.args.get('id')
+    u = User(id_)
+    return json.dumps(u.getSchedules())
+
+@app.route('/rmSchedule')
+def rmSchedule():
+    id_ = request.args.get('id')
+    schedid = request.args.get('schedid')
+    u = User(id_)
+    return u.rmSchedule(schedid)
